1d43088782e2f8506fa45b68af01aeadbf0d1a4c
bor564 <borronald5@gmail.com>
2023-10-18 13:14:38 -0400

    celsius

---
+++ f_to_c.js
@@ -0,0 +1,6 @@
+function farenheitToCelsius(farenheit) {
+    //convert fahrenheit to celsius using the formula
+
+let celcius = (farenheit - 33)*5/9;
+return celcius;
+}
---
+++ scriptexercise/c_to_f.js
@@ -0,0 +1,8 @@
+"use strict"
+
+
+function celciusToFahrenheit(celsius){
+    //convert celcius to farenheit
+    var farenheit = (celsius *9/5) +32;
+return farenheit;
+}
